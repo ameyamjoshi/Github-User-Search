c484fff910b94b92648a3f564e692a76302d8eec
Ameya Joshi <ameya.jshi@gmail.com>
2021-06-23 20:53:29 +0530

    Search User Functionality in progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import User from "./User"
-import SearchData from "./SearchData"
+import logo from "./logo.svg";
+import "./App.css";
+import User from "./User";
+import SearchData from "./SearchData";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h2>Github Users Data using Api</h2> 
-      <SearchData/>
-    <User/>
+      <BrowserRouter> 
+     <Switch>
+        <Route path="/" component={User} exact />
+        <Route path="/searchuser" component={SearchData} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/SearchData.js
+++ src/SearchData.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function SearchData() {
-    return (
-        <div>
-           <input placeholder="type username here"></input>
-        </div>
-    )
+    async function fetchData(ip) {
+        console.log(ip.ip)
+        let x = await fetch(`https://api.github.com/users/${ip.ip}`);
+    
+        setUserInfo(await x.json());
+        console.log({ UserInfo });
+      }
+   
+  const [ip, setip] = useState("");
+  const handleSubmit = (e) => {
+      e.prevetDefault();
+    console.log("Submitted");
+  };
+  useEffect(() => {
+   console.log("UseEffect Called")
+    fetchData({ip});
+   
+}, [ip])
+const [UserInfo, setUserInfo] = useState("")
+  return (
+    <div>
+      <h3>Search User by UserName</h3>
+      <input
+        placeholder='type username here'
+        onChange={(e) => {setip(e.target.value)
+                       console.log(e.target.value) 
+        }}></input>
+      <button onClick={()=>setip(ip)}>Find User</button>
+      {ip}
+      { UserInfo.created_at }
+    </div>
+  );
 }
 
-export default SearchData
+export default SearchData;
